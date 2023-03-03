c66a813cb7f446e142b2e487b230528202944b0f
GasbroNinja <gasbroider@live.it>
2023-03-03 17:15:17 +0100

    Weekly Project U2-W1-D5

---
+++ U2/W1/D5/JS/script.js
@@ -0,0 +1,13 @@
+
+
+function scrollValue() {
+    var navbar = document.getElementById('navbar');
+    var scroll = window.scrollY;
+    if (scroll < 200) {
+        navbar.classList.remove('BgColour');
+    } else {
+        navbar.classList.add('BgColour');
+    }
+}
+
+window.addEventListener('scroll', scrollValue);
