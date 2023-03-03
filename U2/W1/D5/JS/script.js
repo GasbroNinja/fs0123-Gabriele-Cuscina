5119087e0c2d3fde9be5fcc4e047f5eff6bb1c46
GasbroNinja <gasbroider@live.it>
2023-03-03 18:05:13 +0100

    Adjustment Js mediaquey medium and style U2-D5

--- U2/W1/D5/JS/script.js
+++ U2/W1/D5/JS/script.js
@@ -3,11 +3,26 @@
 function scrollValue() {
     var navbar = document.getElementById('navbar');
     var scroll = window.scrollY;
-    if (scroll < 200) {
+    if (scroll < 350) {
         navbar.classList.remove('BgColour');
     } else {
         navbar.classList.add('BgColour');
     }
 }
 
-window.addEventListener('scroll', scrollValue);
+/* DA FIXARE IL BUTTON */
+
+document.addEventListener('scroll', scrollValue);
+
+function scrollValue1() {
+    var navbar = document.getElementById('navbar');
+
+if (scroll < 350) {
+    navbar.classList.remove('ButtonC');
+} else {
+    navbar.classList.remove('ButtonC');
+}
+
+}
+
+document.addEventListener('scroll', scrollValue);
